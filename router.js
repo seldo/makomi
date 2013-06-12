ba829ab9d8ad7c729222242da5258fb1f1c93db3
Laurie <github@seldo.com>
2013-06-11 17:44:20 -0700

    Adding mocha for later, but might prefer tape. Minor changes to makomi-express

--- router.js
+++ router.js
@@ -5,6 +5,24 @@ module.exports = function(app){
     , output = require('./routes/output')
     , toolbox = require('./routes/toolbox')
 
+    app.get('/sessionwrite',function(req, res){
+
+        var sessionData = {
+            "key1": "val" + Math.random(),
+            "key2": "val2"
+        }
+
+        req.session.set('testkey',sessionData,function(){
+            res.render('sessionwrite',{'sessiondata':sessionData});
+        });
+    });
+
+    app.get('/sessionread',function(req, res){
+        req.session.get('testkey',function(er,sessionData){
+            res.render('sessionread',{'sessiondata':sessionData});
+        });
+    });
+
   app.get('/:project', routes.index);
   app.get('/:project/context', context.dom);
   app.get('/:project/output',  output.base);
