10333bb3fc780d8cbdf3587b9c0f7a9c9b4bfc40
Laurie <github@seldo.com>
2013-06-24 12:27:24 -0700

    Wrong turn: template precompiler should be a runtime helper library

--- controllers/context.js
+++ controllers/context.js
@@ -4,17 +4,12 @@
 
 var mkUtil = require('makomi-source-util'),
     hb = require('handlebars'),
-    mkPre = require('makomi-template-precompiler');
+    mkRun = require('makomi-express-runtime');
 
 exports.dom = function(req, res) {
 
   mkUtil.init(appConfig,req.session.project)
   mkUtil.getRoutes(function(routes) {
-
-    var renderFragment = function(source,context) {
-      var template = hb.compile(source);
-      return template(context)
-    }
 
     var layout = {
       source: "layouts/default",
@@ -32,7 +27,7 @@ exports.dom = function(req, res) {
       }
     }
 
-    mkPre.compile(layout,renderFragment,function(renderedView) {
+    mkRun.compile(layout,function(renderedView) {
       res.send(renderedView)
     });
 
---
+++ node_modules/makomi-express-runtime/README.md
@@ -0,0 +1,5 @@
+# Makomi Express runtime library
+
+Write Makomi-compatible templates in a templating language of your choice.
+Makomi Express will compile them into Handlebars templates. The Makomi Express
+Runtime is a set of Handlebars helpers that help render the output.
